f6b257fab45ddaf11db82da2f8a7aa65208edf63
jiwon <zest1923@gmial.com>
2022-08-27 18:50:30 +0900

    [Feat] #3 - Live Camera

--- DetectMedicine/DetectMedicine/Views/MainView/ViewController.swift
+++ DetectMedicine/DetectMedicine/Views/MainView/ViewController.swift
@@ -6,14 +6,99 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var cameraView: UIView!
+    // snapshot
+    var videoDataOutput: AVCaptureVideoDataOutput!
+    var videoDataOutputQueue: DispatchQueue!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var captureDevice: AVCaptureDevice!
+    let session = AVCaptureSession()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setAVCapture()
     }
-
-
+    
+    override var shouldAutorotate: Bool {
+        let orientation = UIDevice.current.orientation
+        let UIOrientation = UIDeviceOrientation.self
+        
+        if orientation == UIOrientation.landscapeLeft ||
+            orientation == UIOrientation.landscapeRight ||
+            orientation == UIOrientation.unknown {
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func didTapSnapButton(_ sender: Any) {
+        print("tapped!")
+        if session.isRunning {
+            DispatchQueue.main.async {
+                self.session.stopRunning()
+                sleep(3)
+                self.session.startRunning()
+            }
+            
+        }
+    }
+    
 }
 
+extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
+    
+    func setAVCapture() {
+        session.sessionPreset = AVCaptureSession.Preset.vga640x480
+        guard let device = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
+                                                   for: .video,
+                                                   position: AVCaptureDevice.Position.back) else {
+            return
+        }
+        captureDevice = device
+        beginSession()
+    }
+    
+    func beginSession() {
+        var deviceInput: AVCaptureDeviceInput!
+        
+        do {
+            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            guard deviceInput != nil else {
+                print("device Input Error")
+                return
+            }
+            
+            if self.session.canAddInput(deviceInput) {
+                self.session.addInput(deviceInput)
+            }
+            
+            videoDataOutput = AVCaptureVideoDataOutput()
+            videoDataOutput.alwaysDiscardsLateVideoFrames = true
+            videoDataOutputQueue = DispatchQueue(label: "VideoDataOutputQueue")
+            videoDataOutput.setSampleBufferDelegate(self, queue: self.videoDataOutputQueue)
+            
+            if session.canAddOutput(self.videoDataOutput) {
+                session.addOutput(self.videoDataOutput)
+            }
+            
+            videoDataOutput.connection(with: .video)?.isEnabled = true
+            
+            previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
+            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
+            
+            let rootLayer: CALayer = cameraView.layer
+            rootLayer.masksToBounds = true
+            previewLayer.frame = rootLayer.bounds
+            rootLayer.addSublayer(self.previewLayer)
+            session.startRunning()
+        } catch let error as NSError {
+            deviceInput = nil
+            print(error.localizedDescription)
+        }
+    }
+    
+}
