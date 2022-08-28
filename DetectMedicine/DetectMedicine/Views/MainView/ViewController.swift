68b002e15061ea8aae5e6ffc5ea193da65037686
JIWON1923 <zest1923@gmail.com>
2022-08-29 03:31:09 +0900

    [Feat] #9 - improve live camera quality

--- DetectMedicine/DetectMedicine/Views/MainView/ViewController.swift
+++ DetectMedicine/DetectMedicine/Views/MainView/ViewController.swift
@@ -12,34 +12,87 @@ import MLKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var cameraView: UIView!
-    // snapshot
-    var videoDataOutput: AVCaptureVideoDataOutput!
-    var videoDataOutputQueue: DispatchQueue!
-    var previewLayer: AVCaptureVideoPreviewLayer!
-    var captureDevice: AVCaptureDevice!
-    let session = AVCaptureSession()
+    // capture
+    var session: AVCaptureSession?
+    let output = AVCapturePhotoOutput()
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    
+    // shutter button
+    private let shutterButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        button.layer.cornerRadius = 50
+        button.layer.borderWidth = 10
+        button.layer.borderColor = UIColor.white.cgColor
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setAVCapture()
-    }
-    
-    override var shouldAutorotate: Bool {
-        let orientation = UIDevice.current.orientation
-        let UIOrientation = UIDeviceOrientation.self
+        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
+        view.addSubview(shutterButton)
+        checkCameraPermissions()
         
-        if orientation == UIOrientation.landscapeLeft ||
-            orientation == UIOrientation.landscapeRight ||
-            orientation == UIOrientation.unknown {
-            return false
-        }
-        return true
+        shutterButton.addTarget(self, action: #selector(didTapTakePhoto), for: .touchUpInside)
     }
     
-    // func recognizeText(_ image: UIImage) {
-    func recognizeText() {
-        let image = UIImage(named: "tyrenol.jpeg")!
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer.frame = view.bounds
+        shutterButton.center = CGPoint(x: view.frame.size.width / 2, y: view.frame.size.height - 100)
+    }
+    
+    // check permission
+    private func checkCameraPermissions() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else {
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    self?.setUpCamera()
+                }
+            }
+        case .restricted:
+            break
+        case .denied:
+            break
+        case .authorized:
+            setUpCamera()
+        @unknown default:
+            break
+     
+        }
+    }
+    
+    private func setUpCamera() {
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            do {
+                let input = try AVCaptureDeviceInput(device: device)
+                if session.canAddInput(input) {
+                    session.addInput(input)
+                }
+                
+                if session.canAddOutput(output) {
+                    session.addOutput(output)
+                }
+                
+                previewLayer.videoGravity = .resizeAspectFill
+                previewLayer.session = session
+                
+                session.startRunning()
+                self.session = session
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+     func recognizeText(_ image: UIImage) {
         let options = KoreanTextRecognizerOptions()
         let koreanTextRecognizer = TextRecognizer.textRecognizer(options: options)
         let visionImage = VisionImage(image: image)
@@ -47,79 +100,29 @@ class ViewController: UIViewController {
         
         koreanTextRecognizer.process(visionImage) { result, error in
             guard error == nil, let result = result else {
-                print(error?.localizedDescription)
+                print(error?.localizedDescription as Any)
                 return
             }
-            print(result.text)
+            dump(result.text)
         }
     }
     
-    @IBAction func didTapSnapButton(_ sender: Any) {
-        print("tapped!")
-        if session.isRunning {
-            DispatchQueue.main.async {
-                self.session.stopRunning()
-                sleep(3)
-                self.session.startRunning()
-            }
-            
-        }
-//        recognizeText(cameraView.takeSnapshot())
-        recognizeText()
+    @objc private func didTapTakePhoto() {
+        output.capturePhoto(with: AVCapturePhotoSettings(), delegate: self)
     }
     
 }
 
-extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    
-    func setAVCapture() {
-        session.sessionPreset = AVCaptureSession.Preset.vga640x480
-        guard let device = AVCaptureDevice.default(AVCaptureDevice.DeviceType.builtInWideAngleCamera,
-                                                   for: .video,
-                                                   position: AVCaptureDevice.Position.back) else {
+extension ViewController: AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard let data = photo.fileDataRepresentation() else {
             return
         }
-        captureDevice = device
-        beginSession()
-    }
-    
-    func beginSession() {
-        var deviceInput: AVCaptureDeviceInput!
         
-        do {
-            deviceInput = try AVCaptureDeviceInput(device: captureDevice)
-            guard deviceInput != nil else {
-                print("device Input Error")
-                return
-            }
-            
-            if self.session.canAddInput(deviceInput) {
-                self.session.addInput(deviceInput)
-            }
-            
-            videoDataOutput = AVCaptureVideoDataOutput()
-            videoDataOutput.alwaysDiscardsLateVideoFrames = true
-            videoDataOutputQueue = DispatchQueue(label: "VideoDataOutputQueue")
-            videoDataOutput.setSampleBufferDelegate(self, queue: self.videoDataOutputQueue)
-            
-            if session.canAddOutput(self.videoDataOutput) {
-                session.addOutput(self.videoDataOutput)
-            }
-            
-            videoDataOutput.connection(with: .video)?.isEnabled = true
-            
-            previewLayer = AVCaptureVideoPreviewLayer(session: self.session)
-            previewLayer.videoGravity = AVLayerVideoGravity.resizeAspect
-            
-            let rootLayer: CALayer = cameraView.layer
-            rootLayer.masksToBounds = true
-            previewLayer.frame = rootLayer.bounds
-            rootLayer.addSublayer(self.previewLayer)
-            session.startRunning()
-        } catch let error as NSError {
-            deviceInput = nil
-            print(error.localizedDescription)
-        }
+        session?.stopRunning()
+        
+        let image = UIImage(data: data)
+        recognizeText(image!)
+        session?.startRunning()
     }
-    
 }
