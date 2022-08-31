7deb94e3b7880792c993294803d95c99d94767d7
JIWON1923 <zest1923@gmail.com>
2022-09-01 04:48:42 +0900

    [Feat] #15 - voice over

--- DetectMedicine/DetectMedicine/Views/Onboarding/FirstPageViewController.swift
+++ DetectMedicine/DetectMedicine/Views/Onboarding/FirstPageViewController.swift
@@ -9,12 +9,17 @@ import UIKit
 
 class FirstPageViewController: UIViewController {
     
+    let appInformation = "약 상자를 촬영하면, 결과를 알려드립니다. 이약머약은 어떠한 정보도 수집하지 않습니다. 하면 화단에 있는 촬영하기 버튼을 통해 약을 인식해보세요. 준비가 되셨다면, 화면 하단에 있는 시작하기 버튼을 눌러보세요."
+
     @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var descriptions: UILabel!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         startButton.layer.masksToBounds = true
         startButton.layer.cornerRadius = 35
+        descriptions.accessibilityLabel = appInformation
     }
     
     @IBAction func didTapStartButton() {
